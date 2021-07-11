f4b167f20ba106d1517ec3e76a3c1578b7c0ed0f
David Carrillo Jr <djcarrillo6@yahoo.com>
2021-07-11 11:44:59 -0700

    Added an algorithm component to the navbar. Created 2 algorithm solution posts for Merge Sort & Quick Sort.

---
+++ pages/algorithm.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+import {
+	Heading,
+	Flex,
+	Stack,
+	Input,
+	InputGroup,
+	InputRightElement,
+} from '@chakra-ui/react';
+
+import Container from '../components/Container';
+import { getAllFilesFrontMatter } from '../lib/mdx';
+import AlgoPost from '../components/AlgoPost';
+
+import { SearchIcon } from '@chakra-ui/icons';
+
+export default function Algorithm({ posts }) {
+	const [searchValue, setSearchValue] = useState('');
+
+	const filteredAlgoPosts = posts
+		.sort(
+			(a, b) =>
+				Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt)),
+		)
+		.filter((frontMatter) =>
+			frontMatter.title.toLowerCase().includes(searchValue.toLowerCase()),
+		);
+
+	return (
+		<>
+			<Head>
+				<title>DJWEBDEV - Algorithm Solution Blog Articles</title>
+			</Head>
+			<Container>
+				<Stack
+					as='main'
+					spacing={8}
+					justifyContent='center'
+					alignItems='flex-start'
+					m='0 auto 4rem auto'
+					maxWidth='700px'>
+					<Flex
+						flexDirection='column'
+						justifyContent='flex-start'
+						alignItems='flex-start'
+						maxWidth='700px'
+						px={4}>
+						<Heading letterSpacing='tight' mb={4} as='h1' size='2xl'>
+							Algorithm Solutions ({posts.length})
+						</Heading>
+						<InputGroup mb={4} mr={4} w='100%'>
+							<Input
+								aria-label='Search by title'
+								placeholder='Search by title'
+								onChange={(e) => setSearchValue(e.target.value)}
+							/>
+							<InputRightElement>
+								<SearchIcon color='gray.300' />
+							</InputRightElement>
+						</InputGroup>
+						{!filteredAlgoPosts.length && 'No posts found :('}
+						{filteredAlgoPosts.map((frontMatter) => (
+							<AlgoPost key={frontMatter.title} {...frontMatter} />
+						))}
+					</Flex>
+				</Stack>
+			</Container>
+		</>
+	);
+}
+
+export async function getStaticProps() {
+	const posts = await getAllFilesFrontMatter('algorithms');
+
+	return { props: { posts } };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,63 @@
-import Head from 'next/head'
-import {
-  useColorMode,
-  Heading,
-  Text,
-  Flex,
-  Stack
-} from '@chakra-ui/react'
-import Container from '../components/Container'
+import Head from 'next/head';
+import { useColorMode, Heading, Text, Flex, Stack } from '@chakra-ui/react';
+import Container from '../components/Container';
 
 export default function Index() {
-  const { colorMode } = useColorMode()
-  const colorSecondary = {
-    light: 'gray.700',
-    dark: 'gray.100'
-  }
-  return (
-    <Container>
-      <Head>
-        <title>DJWEBDEV - Home</title>
-      </Head>
-      <Stack
-        as="main"
-        spacing={8}
-        justifyContent="center"
-        alignItems="flex-start"
-        m="0 auto 4rem auto"
-        maxWidth="700px"
-        px={2}
-      >
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
-          alignItems="flex-start"
-          maxWidth="700px"
-        >
-          <Heading mb={10}>Hi! I'm DJ Carrillo, a software engineer, a tech-entreprenuer, and a former world-class bartender.</Heading>
-          <Text color={colorSecondary[colorMode]}>This is my technical blog, where I will periodically post both technical & non-technical blog <a href="https://mdx-remote-blog.vercel.app/blog" style={{color: 'brown'}} >articles</a> about, web development, mobile development, technology development, cocktail recipes, soft skills, and much, much more.... I'm a gradute of California State University of Long Beach, where I majored in economics. After college, I worked in the hospitallity industry for over 10 years, until I graduted from one of the four original coding bootcamps @ Coding Dojo.  My professional portfolio website holds a collection of some of my most recent applications that I have been developing, and it can be visited by clicking <a href="http://www.djwebdev.io" style={{color: 'red'}} >here</a>; or on the link above.  If you would like to connect with me directly, feel free to send me an email <a href="mailto:djcarrillo06@gmail.com" style={{color: 'blue'}} >here</a>.</Text>
-        </Flex>
-      </Stack>
-    </Container>
-  )
+	const { colorMode } = useColorMode();
+	const colorSecondary = {
+		light: 'gray.700',
+		dark: 'gray.100',
+	};
+	return (
+		<Container>
+			<Head>
+				<title>DJWEBDEV - Blog</title>
+			</Head>
+			<Stack
+				as='main'
+				spacing={8}
+				justifyContent='center'
+				alignItems='flex-start'
+				m='0 auto 4rem auto'
+				maxWidth='700px'
+				px={2}>
+				<Flex
+					flexDirection='column'
+					justifyContent='flex-start'
+					alignItems='flex-start'
+					maxWidth='700px'>
+					<Heading mb={10}>
+						Hi! I'm DJ Carrillo, a software engineer, a tech-entreprenuer, and a
+						former world-class bartender.
+					</Heading>
+					<Text color={colorSecondary[colorMode]}>
+						This is my technical blog, where I will periodically post both
+						technical & non-technical blog{' '}
+						<a
+							href='https://mdx-remote-blog.vercel.app/blog'
+							style={{ color: 'brown' }}>
+							articles
+						</a>{' '}
+						about, web development, mobile development, technology development,
+						cocktail recipes, soft skills, and much, much more.... I'm a gradute
+						of California State University of Long Beach, where I majored in
+						economics. After college, I worked in the hospitallity industry for
+						over 10 years, until I graduted from one of the four original coding
+						bootcamps @ Coding Dojo. My professional portfolio website holds a
+						collection of some of my most recent applications that I have been
+						developing, and it can be visited by clicking{' '}
+						<a href='http://www.djwebdev.io' style={{ color: 'red' }}>
+							here
+						</a>
+						; or on the link above. If you would like to connect with me
+						directly, feel free to send me an email{' '}
+						<a href='mailto:djcarrillo06@gmail.com' style={{ color: 'blue' }}>
+							here
+						</a>
+						.
+					</Text>
+				</Flex>
+			</Stack>
+		</Container>
+	);
 }
